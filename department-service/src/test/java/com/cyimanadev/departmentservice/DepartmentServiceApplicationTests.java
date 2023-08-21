b46edbf666364da0ebc946a2d4bff2a73904015c
CYIMANA-Faisal <cyimanafaisal@gmail.com>
2023-08-21 10:26:53 +0200

    add API gatewa and loadbalanced the request

---
+++ department-service/src/test/java/com/cyimanadev/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyimanadev.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
